0cca4d36368da896bfe931ff58195a30dd3f1ae0
1340691923@qq.com <1340691923@qq.com>
2022-02-25 15:02:21 +0800

    缓存改为并发安全

--- engine/es/EsCache.go
+++ engine/es/EsCache.go
@@ -7,7 +7,7 @@ import (
 
 // Es连接缓存
 type EsCache struct {
-	esConnectMap map[int]*EsConnect
+	esConnectMap *sync.Map
 }
 
 var (
@@ -18,27 +18,30 @@ var (
 // es
 func NewEsCache() *EsCache {
 	once.Do(func() {
-		esCache = &EsCache{esConnectMap: map[int]*EsConnect{}}
+		esCache = &EsCache{esConnectMap: new(sync.Map)}
 	})
 	return esCache
 }
 
 // 新增一个es实例
 func (this *EsCache) Set(id int, esClient *EsConnect) {
-	this.esConnectMap[id] = esClient
+	this.esConnectMap.Store(id, esClient)
 }
 
 // 通过es连接表 的id获取一个保存在内存的id
 func (this *EsCache) Get(id int) *EsConnect {
-	if _, getConnect := this.esConnectMap[id]; getConnect {
-		return this.esConnectMap[id]
+	if v, getConnect := this.esConnectMap.Load(id); getConnect {
+		return v.(*EsConnect)
 	}
 	return nil
 }
 
 // 通过id删除一个内存中的es实例
 func (this *EsCache) Rem(id int) {
-	if _, getConnect := this.esConnectMap[id]; getConnect {
-		delete(this.esConnectMap, id)
-	}
+	this.esConnectMap.Range(func(key, value interface{}) bool {
+		if id == key {
+			this.esConnectMap.Delete(key)
+		}
+		return true
+	})
 }
